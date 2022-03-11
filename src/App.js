a1799b6ee46d77c7acaf02b67a7c9b2e53804ba8
AHeraudMH <aheraudmh@hotmail.com>
2022-03-11 17:55:31 -0500

    [WIP]: Agregar componente contenedor con fondo de imagen

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import PageWrapper from './componentes/PageWrapper'; 
+
+function App() {
+  return (
+    <PageWrapper>
+      
+    </PageWrapper>
+  );
+}
+
+export default App;
